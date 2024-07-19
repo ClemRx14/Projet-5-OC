5029d9b5fc29c0e6a49e2ba4e3e79afc1a3e0f6c
Clement Raux <ilesquafr@gmail.com>
2024-07-19 22:02:09 +0200

    utilisation du tableau

--- assets/script.js
+++ assets/script.js
@@ -23,60 +23,47 @@ let cliquedroit = "vous avez cliqué à droite"
 
 // **** variables concernant les slides *****//
 
-let slideactuel = 0
-const slidetotal = 3
-let bulletactuel = 0
-const bullettotal = 3
-let texteactuel = 0
-const textetotal = 3
-const texteBackground = [
-	"Impressions tous formats <span>en boutique et en ligne</span>",
-	"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-	"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-	"Autocollants <span>avec découpe laser sur mesure</span>"
-];
+let slideActuel = 0
+const slideTotal = 3
+const bulletTotal = 4
+const tabData = slides
 
 let flechedegauche = document.getElementById("flechegauche");
 flechedegauche.addEventListener("click", function() {
 	
-	if (slideactuel > 0) {
-		slideactuel--;
+	if (slideActuel > 0) {
+		slideActuel--;
 	} else {
-		slideactuel = slidetotal;
+		slideActuel = slideTotal;
 	}
 		changementBanniere ();
 		changementBullet ();
 		changementTexte ();
 	
 	console.log(cliquegauche);
-
 });
 
 
 let flechededroite = document.getElementById("flechedroite");
 flechededroite.addEventListener("click", function() {
 	
-	if (slideactuel < slidetotal) {
-		slideactuel++;
+	if (slideActuel < slideTotal) {
+		slideActuel++;
 	} else {
-		slideactuel = 0;
+		slideActuel = 0;
 	}
 		changementBanniere ();
 		changementBullet ();
 		changementTexte ();
 
 	console.log(cliquedroit);
-	
 });
 
 	//***** Fonction pour changer l'image du background ***** //
 
 function changementBanniere () {
 	let banniere = document.getElementById("banniere")
-	banniere.setAttribute('src', 'assets/images/slideshow/slide' + (slideactuel + 1) + '.jpg');
-	if (slideactuel === 3) {
-		banniere.setAttribute('src', 'assets/images/slideshow/slide' + (slideactuel + 1) + '.png');
-	}
+	banniere.setAttribute('src', 'assets/images/slideshow/' + tabData[slideActuel].image);
 }
 
 //***** Fonction pour changer le bullet-point *******/
@@ -84,13 +71,14 @@ function changementBanniere () {
 function changementBullet () {
 	let bulletPoint = document.querySelectorAll(".dot");
 
-	for (let bulletactuel = 0; bulletactuel < bullettotal; bulletactuel++) {
+	for (let bulletActuel = 0; bulletActuel < bulletTotal; bulletActuel++) {
+		let newBullet = bulletPoint[bulletActuel];
 
-	if (bulletactuel === slideactuel) {
-		bulletPoint.classlist.add("dot_selected");
+	if (bulletActuel === slideActuel) {
+		newBullet.classList.add("dot_selected");
 	}
 	else {
-		bulletPoint.classlist.remove("dot_selected");
+		newBullet.classList.remove("dot_selected");
 	}
   }
 }
@@ -99,8 +87,5 @@ function changementBullet () {
 
 function changementTexte () {
 	let description = document.getElementById("description");
-
-	for ( let texteactuel = 0; texteactuel < textetotal; texteBackground++) {
-	description.innerHTML = texteBackground;
-	}
+	description.innerHTML = tabData[slideActuel].tagLine;
 }
